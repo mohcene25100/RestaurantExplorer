d72170abe91ababab70573d42f81d2d8199996e3
mohcene25100 <m.segouat@esi-sba.dz>
2023-03-01 17:53:43 +0100

    Add icons and Stuff

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -4,6 +4,7 @@ import { Card } from 'react-native-paper'
 import styled from 'styled-components'
 import { SvgXml } from 'react-native-svg'
 import star from '../../../../assets/star'
+import openshop from '../../../../assets/open'
 
 
 
@@ -34,18 +35,28 @@ const Rating = styled(View)`
     padding-top:${props => props.theme.space[2]}
     padding-bottom:${props => props.theme.space[2]}
 `
+const Section = styled(View)`
+    flex-direction:row;
+    align-items:center;`
+const SectionEnd = styled(View)`
+    flex:1;
+    flex-direction:row;
+    justify-content:flex-end;
+`
+
+
 
 export const RestaurantInfoCard = ({ restaurant }) => {
 
     //  Destructing
 
     const { name = 'Taco King',
-        icon,
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
         address = '199, Gambita Street, Algiers',
         photos = ["https://i.pinimg.com/736x/eb/e7/65/ebe7655e8047a8635d30e8603dc049ff.jpg", "https://w7.pngwing.com/pngs/686/527/png-transparent-fast-food-hamburger-sushi-pizza-fast-food-food-breakfast-fast-food-restaurant-thumbnail.png", "https://ak-d.tripcdn.com/images/1i65x2215bvic5sjk39C0_R_400_10000_R5_Q90.jpg_.webp?proc=source/trip"],
         isOpenNow = true,
         rating = 3.2,
-        isClosedTemporarily }
+        isClosedTemporarily = true }
         = restaurant
 
     const ratingArray = Array.from(new Array(Math.floor(rating)))
@@ -59,11 +70,19 @@ export const RestaurantInfoCard = ({ restaurant }) => {
                                 <RestaurantCardCover source={{ uri: photo }} />
                                 <Info>
                                     <Title>{`Restaurant ${index + 1}`}</Title>
-                                    <Rating>
-                                        {ratingArray.map((index) => {
-                                            return <SvgXml key={index} xml={star} width={20} height={20} />
-                                        })}
-                                    </Rating>
+                                    <Section>
+                                        <Rating>
+                                            {ratingArray.map((rating, index) => {
+                                                return <SvgXml key={index} xml={star} width={20} height={20} />
+                                            })}
+                                        </Rating>
+                                        <SectionEnd>
+                                            {isClosedTemporarily && <Text variant='label' style={{ color: 'red' }}>CLOSED TEMPORARILY</Text>}
+                                            <View style={{ paddingLeft: 16 }} />
+                                            {isOpenNow && <SvgXml xml={openshop} width={20} height={20} />}
+                                            <Image style={{ marginLeft: 16, width: 15, height: 15 }} source={{ uri: icon }} />
+                                        </SectionEnd>
+                                    </Section>
 
                                     <Address>{address}</Address>
                                 </Info>
