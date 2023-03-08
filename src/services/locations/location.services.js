0e4cf3b2f2b04a7b6b76bf51005bbf204580819d
mohcene25100 <m.segouat@esi-sba.dz>
2023-03-08 10:46:21 +0100

    Update location.services.js

--- src/services/locations/location.services.js
+++ src/services/locations/location.services.js
@@ -14,8 +14,8 @@ export const locationRequest = (searchTerm) => {
 }
 
 export const locationTransform = (result) => {
-
-    const { geometry = {} } = camelize(result.results)[0]
+    const formattedResult = camelize(result)
+    const { geometry = {} } = formattedResult.results[0]
     const { lat, lng } = geometry.location
     return { lat, lng }
 
