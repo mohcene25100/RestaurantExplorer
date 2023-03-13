b4938a5ab664f037fa9d1ea3e01a4eaa4a7322c9
mohcene25100 <m.segouat@esi-sba.dz>
2023-03-13 11:38:53 +0100

    Put Search in Map Screen

--- src/features/maps/components/search.component.js
+++ src/features/maps/components/search.component.js
@@ -8,6 +8,10 @@ import { LocationContext } from '../../../services/locations/location.context'
 
 const SearchContainer = styled(View)`
     padding: ${props => props.theme.space[3]};
+    position:absolute;
+    z-index:999;
+    top:40px;
+    width:100%;
 
 `
 
@@ -15,14 +19,15 @@ export const Search = () => {
 
     const { keyword, search } = useContext(LocationContext)
     const [searchKeyword, setSearchKeyword] = useState(keyword)
+
     useEffect(() => {
         setSearchKeyword(keyword)
     }, [keyword])
 
-
     return (
         <SearchContainer>
             <Searchbar
+                icon='map'
                 placeholder='Search for a location...'
                 onChangeText={(query) => { setSearchKeyword(query) }}
                 value={searchKeyword}
--- src/features/maps/screens/map.screen.js
+++ src/features/maps/screens/map.screen.js
@@ -1,8 +1,19 @@
 import MapView from "react-native-maps"
+import styled, { StyledComponent } from "styled-components"
+import { Search } from "../components/search.component"
 
+const Map = styled(MapView)`
 
+    height:100%;
+    width:100%;
+
+`
 export const MapScreen = () => {
     return (
-        <MapView style={{ height: '100%' }} />
+        <>
+            <Search />
+            <Map />
+        </>
+
     )
 }
