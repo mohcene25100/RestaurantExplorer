fcd6183d11cd7ddc9fdecbad7aa078e50cbb5f1a
mohcene25100 <m.segouat@esi-sba.dz>
2023-03-14 11:26:01 +0100

    Delete useless lines

--- src/components/restaurants/compact-restaurant-info.js
+++ src/components/restaurants/compact-restaurant-info.js
@@ -1,21 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { WebView } from 'react-native-webview';
-import { Platform, Image, View } from "react-native";
+import { Image, View } from "react-native";
 
 import { Text } from "../typography/text.component";
 
-const CompactImage = styled(Image)`
-  border-radius: 10px;
-  width: 120px;
-  height: 100px;
-`;
 
-const CompactWebview = styled(WebView)`
-  border-radius: 10px;
-  width: 120px;
-  height: 100px;
-`;
 
 const Item = styled(View)`
   padding: 10px;
@@ -23,10 +12,8 @@ const Item = styled(View)`
   align-items: center;
 `;
 
-const isAndroid = Platform.OS === "android";
 
 export const CompactRestaurantInfo = ({ restaurant }) => {
-  // const Imagee = isAndroid ? CompactWebview : CompactImage;
   return (
     <Item>
       <Image style={{ width: 100, height: 100 }} source={{
